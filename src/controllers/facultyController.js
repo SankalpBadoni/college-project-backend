e984e88849da0805c7aae5f045caa3a4bacca67d
Ayush Kumar <ayush51501kumar@gmail.com>
2026-05-19 12:58:38 +0530

    .

--- src/controllers/facultyController.js
+++ src/controllers/facultyController.js
@@ -337,11 +337,12 @@ export const uploadGenericFileController = async (req, res, next) => {
       return res.status(400).json({ message: "No file uploaded" });
     }
 
+    const userId = req.faculty?._id || req.employer?._id || req.user?._id || 'anonymous';
     const uploadResult = await uploadBufferToS3({
       buffer: req.file.buffer,
       originalName: req.file.originalname,
       mimeType: req.file.mimetype,
-      folder: `course-assets/${req.faculty._id}`
+      folder: `assets/${userId}`
     });
 
     return res.json({ fileUrl: uploadResult.fileUrl });
